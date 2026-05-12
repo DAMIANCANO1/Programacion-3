2c7b31c980a267745c0263bcaafde3631c988a3c
canod <canod@LAPTOP-9FR7JI32.home>
2026-05-12 10:40:57 -0600

    Programacion

---
+++ src/Estructuras/ArrayListBasics.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Estructuras;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Collections;
+import java.util.List;
+
+public class ArrayListBasics {
+    public static void main (String args[]){
+        
+        
+    }
+    
+}
