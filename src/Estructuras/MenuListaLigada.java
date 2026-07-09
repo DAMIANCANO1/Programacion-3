a910dbfec5e1553586aacca2fc65335f3578215a
canod <canod@LAPTOP-9FR7JI32.home>
2026-07-09 11:32:44 -0600

    PROGRAMACION 3 LISTAS LIGADAS

--- src/Estructuras/MenuListaLigada.java
+++ src/Estructuras/MenuListaLigada.java
@@ -4,16 +4,47 @@
  */
 package Estructuras;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author canod
  */
 public class MenuListaLigada {
+    
     public static void main(String[] args) {
-        while (true) {  
+        while (true) {
+            int tamal = Integer.parseInt(JOptionPane.showInputDialog(null,
+                    "Ingresa una opcion: \n 1. Lista simple\n 2.Lista con objetos"
+                    + "\n 0. Salir"));
             
+            switch (tamal) {
+                //caso 1 listas simples.
+                case 1:
+                    int opcionS = Integer.parseInt(JOptionPane.showInputDialog(null,
+                            "Ingresa la opcion a realizar "
+                            + "\n 1. String "
+                            + "\n 2. Integer"
+                            + "\n 3. double "));
+                    break;
+
+                //caso 2 lista con objetos     
+                case 2:
+                    break;
+
+                //caso 0 para salir del menu     
+                case 0:
+                    break;
+                
+                default:
+                
+            }
             
         }
+        
+    }
+    
+    public void ListaSimple(int opcionS) {
     }
     
 }
