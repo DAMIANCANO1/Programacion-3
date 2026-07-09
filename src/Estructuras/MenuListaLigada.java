b5a9adebc5f480b86cf8e0e44280ab5cd69ed1ad
canod <canod@LAPTOP-9FR7JI32.home>
2026-07-09 11:07:46 -0600

    PROGRAMACION 3 LISTAS LIGADAS

---
+++ src/Estructuras/MenuListaLigada.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Estructuras;
+
+/**
+ *
+ * @author canod
+ */
+public class MenuListaLigada {
+    public static void main(String[] args) {
+        while (true) {  
+            
+            
+        }
+    }
+    
+}
